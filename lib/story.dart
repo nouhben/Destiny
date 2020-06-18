b380e33f90197c2eb230eb851451116bb59eb7ac
nouh ben kadi <benkadi.nouh@icloud.com>
2020-06-18 13:56:40 +0100

    All TODOs are done

--- lib/story.dart
+++ lib/story.dart
@@ -1,18 +1,17 @@
-//TODO: Step 2 - Create a new class called Story.
 class Story {
   String _storyTitle;
   String _choice1;
   String _choice2;
 
-  Story({String title, String choice1, String choice2}) {
-    _storyTitle = title;
+  Story({String storyTitle, String choice1, String choice2}) {
+    _storyTitle = storyTitle;
     _choice1 = choice1;
     _choice2 = choice2;
   }
-}
-//TODO: Step 3 - Create 3 properties for this class,
-//TODO: Step 3 - A. storyTitle to store the story text.
-//TODO: Step 3 - B. choice1 to store the text for choice 1,
-//TODO: Step 3 - C. choice2 to store the text for choice 2.
 
-//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
+  String get storyTitle => _storyTitle;
+
+  String get choice2 => _choice2;
+
+  String get choice1 => _choice1;
+}
